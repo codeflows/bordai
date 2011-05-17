2baf6681bd8de4e10c73b1df454359b4d1359d31
Kai Rasi <kai.rasi@reaktor.fi>
2011-05-17 09:43:52 +0300

    working after refactorings

--- src/targeterApp.cpp
+++ src/targeterApp.cpp
@@ -1,15 +1,9 @@
 #include "cinder/app/AppBasic.h"
-#include "cinder/Surface.h"
-#include "cinder/gl/Texture.h"
-#include "cinder/Capture.h"
-#include "cinder/Text.h"
-#include "CinderOpenCv.h"
 
 #include "CapturingDevice.h"
 
 #define WIDTH 640
 #define HEIGHT 480
-#define HISTOGRAM_SCALE 2
 
 using namespace ci;
 using namespace ci::app;
@@ -20,29 +14,22 @@ class targeterApp : public AppBasic {
 	void prepareSettings(Settings *settings);
 	void setup();
 	void keyDown( KeyEvent event );
-	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
-	void updateTargets( Surface cameraImage );
 	
   private:
 	CapturingDevice mCamera;
-	vector<gl::Texture>	mTextures;
-	vector<gl::Texture>	mNameTextures;
-	vector<Surface>		mRetainedSurfaces;
-	cv::CascadeClassifier mCascade;
-	vector<Rectf>		mTargets;
 };
 
 void targeterApp::prepareSettings(Settings *settings) {
-	settings -> setWindowSize(WIDTH, HEIGHT);
 	settings -> setFrameRate(60.0f);
+	settings -> setWindowSize(WIDTH, HEIGHT);
 	mCamera.setLensSize(WIDTH, HEIGHT);
+	mCamera.setImageScanner(ImageScanner( getResourcePath( "aGest.xml" ) ));
 }
 
 void targeterApp::setup() {
-	mCascade.load( getResourcePath( "haarcascade_frontalface_alt.xml" ) );
-	mCamera.startCapturing();	
+	mCamera.startCapturing();
 }
 
 void targeterApp::keyDown( KeyEvent event ) {
@@ -54,38 +41,8 @@ void targeterApp::keyDown( KeyEvent event ) {
 	}
 }
 
-void targeterApp::mouseDown( MouseEvent event ) {}
-
-void targeterApp::update() {
-	
-	for( vector<Capture>::iterator cIt = mCamera.mCaptures.begin(); cIt != mCamera.mCaptures.end(); ++cIt ) {
-		if( cIt->checkNewFrame() ) {
-			Surface8u surf = cIt->getSurface();
-			mTextures[cIt - mCamera.mCaptures.begin()] = gl::Texture( surf );
-			updateTargets( surf );
-		}
-	}
-}
-	
-void targeterApp::updateTargets( Surface cameraImage ) {
-	cv::Mat grayCameraImage( toOcv( cameraImage, CV_8UC1 ) );
-	int histogramWidth = cameraImage.getWidth() / HISTOGRAM_SCALE;
-	int histogramHeigth = cameraImage.getHeight() / HISTOGRAM_SCALE;
-	cv::Mat histogramImage( histogramHeigth, histogramWidth, CV_8UC1);
-	cv::resize( grayCameraImage, histogramImage, histogramImage.size(), 0, 0, cv::INTER_LINEAR );
-	
-	cv::equalizeHist( histogramImage, histogramImage );
-	
-	mTargets.clear();
-	
-	vector<cv::Rect> targets;
-	mCascade.detectMultiScale( histogramImage, targets );
-	
-	for (vector<cv::Rect>::const_iterator targetsIter = targets.begin(); targetsIter != targets.end(); ++targetsIter) {
-		Rectf targetRect( fromOcv( *targetsIter ) );
-		targetRect *= HISTOGRAM_SCALE;
-		mTargets.push_back( targetRect );
-	}
+void targeterApp::update() {	
+	mCamera.bufferCaptured();
 }
 
 void targeterApp::draw()
@@ -93,40 +50,9 @@ void targeterApp::draw()
 	gl::enableAlphaBlending();
 	gl::clear( Color::black() );
 	
-	if( mCamera.mCaptures.empty() )
-		return;
-	
-	float width = getWindowWidth() / mCamera.mCaptures.size();	
-	float height = width / ( WIDTH / (float)HEIGHT );
-	float x = 0, y = ( getWindowHeight() - height ) / 2.0f;
-	
-	for( vector<Capture>::iterator cIt = mCamera.mCaptures.begin(); cIt != mCamera.mCaptures.end(); ++cIt ) {	
-		// draw the latest frame
-		gl::color( Color::white() );
-		if( mTextures[cIt-mCamera.mCaptures.begin()] ) {
-			gl::Texture latestFromCamera = mTextures[cIt-mCamera.mCaptures.begin()];
-			gl::draw(latestFromCamera , Rectf( x, y, x + width, y + height ) );
-			latestFromCamera.disable();
-			for (vector<Rectf>::const_iterator targetIter = mTargets.begin(); targetIter != mTargets.end(); ++targetIter) {
-				gl::color( ColorA( 0, 0, 1, 0.35f ) );
-				gl::drawSolidRect( *targetIter );
-				gl::color( ColorA( 1, 1, 0, 0.45f ) );
-				Rectf rec = *targetIter;
-				float scalex = getWindowWidth() / (float)WIDTH;
-				float scaley = getWindowHeight() / (float)HEIGHT;
-				rec.set(rec.getX1() * scalex, rec.getY1() * scaley, rec.getX2() * scalex, rec.getY2() * scaley);
-				gl::drawSolidRect( rec );
-			}
-		}
-		
-		// draw the name
-		gl::color( Color::black() );	
-		gl::draw( mCamera.mDeviceNameTextures[cIt-mCamera.mCaptures.begin()], Vec2f( x + 10 + 1, y + 10 + 1 ) );
-		gl::color( Color( 0.5, 0.75, 1 ) );
-		gl::draw( mCamera.mDeviceNameTextures[cIt-mCamera.mCaptures.begin()], Vec2f( x + 10, y + 10 ) );
-		
-		x += width;
-	}	
+	if( mCamera.hasSomething() ) {
+		mCamera.draw();
+	}
 }
 
 
--- xcode/CapturingDevice.cpp
+++ xcode/CapturingDevice.cpp
@@ -1,24 +1,20 @@
-/*
- *  CapturingDevice.cpp
- *  targeter
- *
- *  Created by Kai Rasi on 5/15/11.
- *  Copyright 2011 Reaktor Innovations. All rights reserved.
- *
- */
 #include "cinder/app/AppBasic.h"
 #include "cinder/Text.h"
+#include "cinder/Surface.h"
 #include "CapturingDevice.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-CapturingDevice::CapturingDevice() {}
-
 void CapturingDevice::setLensSize(const int lensWidth, const int lensHeight) {
 	mLensWidth = lensWidth;
 	mLensHeight = lensHeight;
+}
+
+void CapturingDevice::setImageScanner( ImageScanner scanner ) {
+	mImageScanner = scanner;
+	mImageScanner.setImageSize(mLensWidth, mLensHeight);
 }
 
 void CapturingDevice::startCapturing() {
@@ -28,13 +24,12 @@ void CapturingDevice::startCapturing() {
 		console() << "Located device " << device->getName() << " with id " << device->getUniqueId() << std::endl;
 		try {
 			if (device->checkAvailable()) {
-				// bindNameTexture(device);
 				mCaptures.push_back( Capture( mLensWidth, mLensHeight, device ) );
 				mCaptures.back().start();
+				mTextureBuffer.push_back( gl::Texture() );
 
-				// bindCapture(device);
 				TextLayout layout;
-				layout.setFont( Font( "Arial", 16 ) );
+				layout.setFont( Font( "Arial", 18 ) );
 				layout.setColor( Color( 1, 1, 0 ) );
 				layout.addLine( device->getName() );
 				mDeviceNameTextures.push_back( gl::Texture( layout.render( true ) ) );				
@@ -43,45 +38,55 @@ void CapturingDevice::startCapturing() {
 		catch (CaptureExc &) {
 			console() << "Cannot use device " << device->getName() << std::endl;
 		}
-	}		
+	}
+}
+
+void CapturingDevice::bufferCaptured() {
+	for( vector<Capture>::const_iterator aCapture = mCaptures.begin(); aCapture != mCaptures.end(); ++aCapture ) {
+		if( aCapture->checkNewFrame() ) {
+			Surface8u capturedSurface = aCapture->getSurface();
+			mTextureBuffer[aCapture - mCaptures.begin()] = gl::Texture( capturedSurface );
+			mImageScanner.scan(capturedSurface);
+		}
+	}	
+}
+
+void CapturingDevice::draw() {
+	float width = getWindowWidth() / mCaptures.size();	
+	float height = width / ( mLensWidth / (float)mLensHeight );
+	float x = 0, y = ( getWindowHeight() - height ) / 2.0f;	
+	
+	for( vector<Capture>::iterator aCapture = mCaptures.begin(); aCapture != mCaptures.end(); ++aCapture ) {	
+		int frameIndex = aCapture-mCaptures.begin();
+		// draw the latest frame
+		gl::color( Color::white() );
+		if( mTextureBuffer[frameIndex] ) {
+			gl::Texture cameraTexture = mTextureBuffer[frameIndex];
+			gl::draw(cameraTexture , Rectf( x, y, x + width, y + height ) );
+			cameraTexture.disable();
+			mImageScanner.draw();
+		}
+		
+		// draw the name
+		gl::color( Color::black() );	
+		gl::draw( mDeviceNameTextures[frameIndex], Vec2f( x + 10 + 1, y + 10 + 1 ) );
+		gl::color( Color( 0.5, 0.75, 1 ) );
+		gl::draw( mDeviceNameTextures[frameIndex], Vec2f( x + 10, y + 10 ) );
+		
+		x += width;
+	}	
 }
 
 void CapturingDevice::toggleOnOff() {
 	mCaptures.back().isCapturing() ? mCaptures.back().stop() : mCaptures.back().start();
 }
 
+bool CapturingDevice::hasSomething() {
+	return !mCaptures.empty();
+}
 
-/*
-void bindCapturingToAvailableDevices() {
-
-		*/
-	/*
-	 // list out the devices
-	 vector<Capture::DeviceRef> devices( Capture::getDevices() );
-	 for( vector<Capture::DeviceRef>::const_iterator deviceIt = devices.begin(); deviceIt != devices.end(); ++deviceIt ) {
-	 Capture::DeviceRef device = *deviceIt;
-	 console() << "Found Device " << device->getName() << " ID: " << device->getUniqueId() << std::endl;
-	 try {
-	 if( device->checkAvailable() ) {
-	 mCaptures.push_back( Capture( WIDTH, HEIGHT, device ) );
-	 mCaptures.back().start();
-	 
-	 // placeholder text
-	 mTextures.push_back( gl::Texture() );
-	 
-	 // render the name as a texture
-	 TextLayout layout;
-	 layout.setFont( Font( "Arial", 24 ) );
-	 layout.setColor( Color( 1, 1, 1 ) );
-	 layout.addLine( device->getName() );
-	 mNameTextures.push_back( gl::Texture( layout.render( true ) ) );
-	 }
-	 console() << "device is NOT available" << std::endl;
-	 }
-	 catch( CaptureExc & ) {
-	 console() << "Unable to initialize device: " << device->getName() << endl;
-	 }
-	 }	
-	 
-*/
-//}
+inline DeviceName {
+	
+	void draw() {
+	}
+}
